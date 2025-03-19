59a6d278b87cd515fdcd8dfb4547ba22e27f1cee
mkhotamirais <mkhotamirais@gmail.com>
2025-03-19 11:44:14 +0700

    create vercel.json'

--- config/db.js
+++ config/db.js
@@ -1,12 +1,23 @@
 import mysql from "mysql2/promise";
 import "dotenv/config";
 
-const dbConfig = {
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-};
+let dbConfig = {};
+
+if (process.env.NODE_ENV === "production") {
+  dbConfig = {
+    host: process.env.DB_HOST_PROD,
+    user: process.env.DB_USER_PROD,
+    password: process.env.DB_PASS_PROD,
+    database: process.env.DB_NAME_PROD,
+  };
+} else {
+  dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+  };
+}
 
 const db = mysql.createPool(dbConfig);
 
--- config/origins.js
+++ config/origins.js
@@ -1,5 +1,6 @@
 export const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
-  "https://mkhotami-react-api.vercel.app",
+  "https://react-api-mkhotami.vercel.app",
+  "https://react-api.mkhotami.com",
 ];
