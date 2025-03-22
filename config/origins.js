1d33390e769c615d32fc2d91bfa04c2db03a4736
mkhotamirais <mkhotamirais@gmail.com>
2025-03-22 22:13:57 +0700

    add origin

--- config/origins.js
+++ config/origins.js
@@ -3,4 +3,6 @@ export const allowedOrigins = [
   "http://localhost:5174",
   "https://react-api-mkhotami.vercel.app",
   "https://react-api.mkhotami.com",
+  "https://vue-mkhotami.vercel.app",
+  "https://vue.mkhotami.com",
 ];
