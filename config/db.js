4da324b8217ab96e082715a8991444833d748ee6
mkhotamirais <mkhotamirais@gmail.com>
2025-06-19 21:17:50 +0700

    err2

--- config/db.js
+++ config/db.js
@@ -1,13 +1,23 @@
 import mysql from "mysql2/promise";
 import "dotenv/config";
 
-const isDev = process.env.NODE_ENV === "development";
-const host = isDev ? process.env.DB_HOST_DEV : process.env.DB_HOST;
-const user = isDev ? process.env.DB_USER_DEV : process.env.DB_USER;
-const password = isDev ? process.env.DB_PASS_DEV : process.env.DB_PASS;
-const database = isDev ? process.env.DB_NAME_DEV : process.env.DB_NAME;
+let dbConfig = {};
 
-const dbConfig = { host, user, password, database };
+if (process.env.NODE_ENV === "production") {
+  dbConfig = {
+    host: process.env.DB_HOST_PROD,
+    user: process.env.DB_USER_PROD,
+    password: process.env.DB_PASS_PROD,
+    database: process.env.DB_NAME_PROD,
+  };
+} else {
+  dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+  };
+}
 
 const db = mysql.createPool(dbConfig);
 
